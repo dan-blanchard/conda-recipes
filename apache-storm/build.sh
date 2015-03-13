90caf830e0f9f3b05c3fb9b7b0e13d7ac2dfe22e
Daniel Blanchard <dblanchard@ets.org>
2015-03-13 11:34:49 -0400

    Fix CLASSPATH issues with Storm and ZK recipes

--- apache-storm/build.sh
+++ apache-storm/build.sh
@@ -8,12 +8,17 @@ cp -R $SRC_DIR/* $PREFIX/storm
 mkdir -p "$PREFIX/bin"
 cd "$PREFIX/bin"
 for filename in ../storm/bin/*; do
-	ln -s $filename $(basename $filename)
+    ln -s $filename $(basename $filename)
 done
+
+# Fix python path in $PREFIX/bin/storm
+sed -i.bak "s|/usr/bin/python|/usr/bin/env python|" $PREFIX/storm/bin/storm
 
 # Make symlinks so that things are in the prefix's lib directory
 mkdir -p "$PREFIX/lib"
 cd "$PREFIX/lib"
 for filename in ../storm/lib/*; do
-	ln -s $filename $(basename $filename)
+    if [ ${filename: -4} != ".jar" ]; then
+       ln -s $filename $(basename $filename)
+    fi
 done
